9865c82b2936f59e9320f750cb679571d9b9d134
Felipe Souza <fnasci@gmail.com>
2021-03-10 20:16:34 -0300

    first commit

---
+++ src/models/Appointment.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    provider: string;
+
+    @Column('time with time zone')
+    date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+};
+
+export default Appointment;
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Request, Response, Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request: Request, response: Response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request: Request, response: Response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const createAppointment = new CreateAppointmentService();
+
+        const appointment = await createAppointment.execute({ date: parsedDate, provider });
+
+        return response.json(appointment);
+
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    };
+});
+
+export default appointmentsRouter;
